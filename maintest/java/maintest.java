5a7d74932f1c1002a80ee94651ba115a5fb47314
IT20651060 <it20651060@my.sliit.lk>
2021-08-27 17:53:38 +0530

    commit

---
+++ maintest/java/maintest.java
@@ -0,0 +1,10 @@
+package maintest.java;
+
+public class maintest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("try 1");
+	}
+
+}
---
+++ maintest/java/test.java
@@ -0,0 +1,5 @@
+package maintest.java;
+
+public class test {
+	
+}
